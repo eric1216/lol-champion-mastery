c8e476c754312fffcd52790c82ce07a7c7249754
Eric H <ericolebusiness@gmail.com>
2022-06-22 22:16:15 -0400

    commit

--- main.py
+++ main.py
@@ -13,12 +13,12 @@ champions_version = versions['n']['champion']
 #get raw champion data
 raw_champion_data = lol_watcher.data_dragon.champions(champions_version)
 
-#itterate over the raw champion data to create a list of only champion ids and champion names
+#iterate over the raw champion data to create a list of only champion ids and champion names
 champ_keys = []*2
 for key, value in raw_champion_data['data'].items():
     champ_keys.append([int(value['key']), key])
 
-#create a datafrom from champ_keys list
+#create a dataframe from champ_keys list
 champion_keys = pd.DataFrame(champ_keys, columns=['championId', 'champion'])
 
 #get champion mastery data
@@ -27,10 +27,10 @@ raw_champion_mastery = lol_watcher.champion_mastery.by_summoner(my_region, me['i
 #create a dataframe from the raw champion mastery data
 champion_mastery = pd.DataFrame(raw_champion_mastery)
 
-#join champion_mastery and champion_keys dataframes to add champion names
+#join champion_mastery and champion_keys dataframes to add champion names column
 final_champion_mastery = pd.merge(champion_mastery, champion_keys, how='inner')
 
-#move champion column to the first columm, drop summonerId, sort by championPoints desc
+#move champion column to the first column, drop summonerId, sort by championPoints desc
 champ_name = final_champion_mastery.pop('champion')
 final_champion_mastery.insert(0, 'champion', champ_name)
 final_champion_mastery = final_champion_mastery.drop(columns='summonerId')
