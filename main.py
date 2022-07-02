f7c0a27e4e497e5cf87938dc0d482837dd5e7b21
Eric H <ericolebusiness@gmail.com>
2022-07-02 15:26:43 -0400

    cleaned comments and adjusted variable names

--- main.py
+++ main.py
@@ -1,41 +1,37 @@
 from riotwatcher import LolWatcher
-import config
 import pandas as pd
+import config
 
 lol_watcher = LolWatcher(config.api_key)
 my_region = 'na1'
 me = lol_watcher.summoner.by_name(my_region, 'ericole')
 
-#get regions current game version
 versions = lol_watcher.data_dragon.versions_for_region(my_region)
 champions_version = versions['n']['champion']
 
-#get raw champion data
+# create dataframe for champions key and name only
 raw_champion_data = lol_watcher.data_dragon.champions(champions_version)
 
-#iterate over the raw champion data to create a list of only champion ids and champion names
-champ_keys = []*2
+champion_keys_list = []*2
 for key, value in raw_champion_data['data'].items():
-    champ_keys.append([int(value['key']), key])
+    champion_keys_list.append([int(value['key']), key])
 
-#create a dataframe from champ_keys list
-champion_keys = pd.DataFrame(champ_keys, columns=['championId', 'champion'])
+champion_keys = pd.DataFrame(champion_keys_list, columns=['championId', 'champion'])
 
-#get champion mastery data
-raw_champion_mastery = lol_watcher.champion_mastery.by_summoner(my_region, me['id'])
+# create dataframe for champion mastery data 
+raw_mastery_data = lol_watcher.champion_mastery.by_summoner(my_region, me['id'])
 
-#create a dataframe from the raw champion mastery data
-champion_mastery = pd.DataFrame(raw_champion_mastery)
+champion_mastery = pd.DataFrame(raw_mastery_data)
 
-#join champion_mastery and champion_keys dataframes to add champion names column
+# join dataframes, rearrange columns, drop "summonerID", sort by "championPoints"
 final_champion_mastery = pd.merge(champion_mastery, champion_keys, how='inner')
 
-#move champion column to the first column, drop summonerId, sort by championPoints desc
 champ_name = final_champion_mastery.pop('champion')
 final_champion_mastery.insert(0, 'champion', champ_name)
+
 final_champion_mastery = final_champion_mastery.drop(columns='summonerId')
+
 final_champion_mastery.sort_values(by='championPoints', ascending=False)
 
-#save dataframe to csv
+# export final dataframe to csv
 final_champion_mastery.to_csv('champion_masteries.csv', index=False)
-
